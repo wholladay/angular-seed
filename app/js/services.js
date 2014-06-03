c6be65cb894943af65d8abfa7abfe13ee362c276
Walter Holladay <walter@holladays.org>
2014-06-03 15:05:36 -0600

    Created a service to get the videos.

--- app/js/services.js
+++ app/js/services.js
@@ -1,9 +1,28 @@
-'use strict';
-
 /* Services */
-
 
 // Demonstrate how to register services
 // In this case it is a simple value service.
 angular.module('myApp.services', []).
-  value('version', '0.1');
+        value('version', '0.2');
+
+myApp.myServices = angular.module('myApp.services');
+
+myApp.myServices.service('VideoService', ['$http', '$q', function($http, $q) {
+
+    return {
+
+        getVideos: function() {
+
+            var deferred = $q.defer();
+            var url = 'http://localhost:8282/movies-lite/video';
+            $http.get(url).success(function(data, status, headers, config) {
+
+                deferred.resolve(data.videos);
+            }).error(function(data, status, headers, config) {
+
+                deferred.reject(data, status, headers, config);
+            });
+            return deferred.promise;
+        }
+    }
+}]);
