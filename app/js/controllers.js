876652be3f1aed6571bdf46fc3948feea7b08871
Walter Holladay <walter@holladays.org>
2014-06-04 08:08:29 -0600

    Movies Controller displays movies in a table.

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,11 +1,34 @@
-'use strict';
-
 /* Controllers */
 
-angular.module('myApp.controllers', [])
-  .controller('MyCtrl1', ['$scope', function($scope) {
+angular.module('myApp.controllers', []);
 
-  }])
-  .controller('MyCtrl2', ['$scope', function($scope) {
+myApp.myControllers = angular.module('myApp.controllers');
 
-  }]);
+myApp.myControllers.controller('HomeController', ['$scope', function($scope) {
+
+}]);
+
+myApp.myControllers.controller('MoviesController', ['$scope', 'VideoService', function($scope, VideoService) {
+
+    function _getVideos() {
+
+        VideoService.getVideos().then(function(videos) {
+            // SUCCESS
+            $scope.movies.videos = videos;
+        }, function(data, status, headers, config) {
+            // FAILURE
+            alert('Oops! something went wrong: ' + status);
+        });
+    }
+
+    function _init() {
+
+        $scope.movies = {
+            videos: null
+        };
+
+        _getVideos();
+    }
+
+    _init();
+}]);
