8eb9066a90fbde16f3ad201e5fb105c0e9248663
Walter Holladay <walter@holladays.org>
2014-06-04 09:39:42 -0600

    Communicate between controllers.

--- app/js/services.js
+++ app/js/services.js
@@ -1,22 +1,25 @@
 /* Services */
 
 // Demonstrate how to register services
+myApp.myServices = angular.module('myApp.services', []);
+
 // In this case it is a simple value service.
-angular.module('myApp.services', []).
-        value('version', '0.3');
+myApp.myServices.value('version', '0.4');
 
-myApp.myServices = angular.module('myApp.services');
-
-myApp.myServices.service('VideoService', ['$http', '$q', function($http, $q) {
+myApp.myServices.service('VideoService', ['$http', '$q', '$rootScope', function($http, $q, $rootScope) {
 
     return {
+        movieCount: '?',
 
         getVideos: function() {
 
             var deferred = $q.defer();
             var url = 'http://localhost:8282/movies-lite/video';
+            var self = this;
             $http.get(url).success(function(data, status, headers, config) {
 
+                self.movieCount = data.videos.length;
+                $rootScope.$broadcast('movieCount', self.movieCount);
                 deferred.resolve(data.videos);
             }).error(function(data, status, headers, config) {
 
